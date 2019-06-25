c2f29ecf089afeb6e4cfd72e2dc6da850e9eb51a
Renzo <renzocand@gmail.com>
2019-06-24 20:04:18 -0500

    clau6

--- public/claudia/assets/js/main.js
+++ public/claudia/assets/js/main.js
@@ -1,33 +1,10 @@
- AOS.init({
- 	duration: 800,
- 	easing: 'slide'
- });
+function principalMain() {
 
-(function($) {
+	$(window).load(function() {
+		
 
 	"use strict";
 
-	$(window).stellar({
-    responsive: true,
-    parallaxBackgrounds: true,
-    parallaxElements: true,
-    horizontalScrolling: false,
-    hideDistantElements: false,
-    scrollProperty: 'scroll'
-  });
-
-
-	var fullHeight = function() {
-
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
-
-	};
-	// fullHeight();
-
-	// loader
 	var loader = function() {
 		setTimeout(function() { 
 			if($('#ftco-loader').length > 0) {
@@ -37,8 +14,6 @@
 	};
 	loader();
 
-	// Scrollax
-//    $.Scrollax();
 
 
    var burgerMenu = function() {
@@ -86,106 +61,7 @@
 
 	};
 	mobileMenuOutsideClick();
+	});
+};
 
-	var carousel = function() {
-		$('.home-slider').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:0,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:false,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:1
-	      },
-	      1000:{
-	        items:1
-	      }
-	    }
-		});
-
-	};
-	// carousel();
-
-	
-
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.ftco-animate').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
-				
-				i++;
-
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-
-					$('body .ftco-animate.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn ftco-animated');
-							} else if ( effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft ftco-animated');
-							} else if ( effect === 'fadeInRight') {
-								el.addClass('fadeInRight ftco-animated');
-							} else {
-								el.addClass('fadeInUp ftco-animated');
-							}
-							el.removeClass('item-animate');
-						},  k * 50, 'easeInOutExpo' );
-					});
-					
-				}, 100);
-				
-			}
-
-		} , { offset: '95%' } );
-	};
-	// contentWayPoint();
-
-
-	// magnific popup
-// 	$('.image-popup').magnificPopup({
-//     type: 'image',
-//     closeOnContentClick: true,
-//     closeBtnInside: false,
-//     fixedContentPos: true,
-//     mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-//      gallery: {
-//       enabled: true,
-//       navigateByImgClick: true,
-//       preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-//     },
-//     image: {
-//       verticalFit: true
-//     },
-//     zoom: {
-//       enabled: true,
-//       duration: 300 // don't foget to change the duration also in CSS
-//     }
-//   });
-
-//   $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-//     disableOn: 700,
-//     type: 'iframe',
-//     mainClass: 'mfp-fade',
-//     removalDelay: 160,
-//     preloader: false,
-
-//     fixedContentPos: false
-//   });
-
-
-
-
-})(jQuery);
-
+principalMain() 
