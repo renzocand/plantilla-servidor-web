0ebf7d30935d17b255316927acf705077c229625
Renzo <renzocand@gmail.com>
2019-06-24 21:31:00 -0500

    clau8

--- public/claudia/assets/js/main.js
+++ public/claudia/assets/js/main.js
@@ -2,20 +2,6 @@ function principalMain() {
 
 	$(window).load(function() {
 		
-
-	"use strict";
-
-	var loader = function() {
-		setTimeout(function() { 
-			if($('#ftco-loader').length > 0) {
-				$('#ftco-loader').removeClass('show');
-			}
-		}, 1);
-	};
-	loader();
-
-
-
    var burgerMenu = function() {
 
 		$('.js-colorlib-nav-toggle').on('click', function(event){
